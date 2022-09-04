835f92437d68d4381f2a8ad5f66a63ac8f2bd316
unknown <vanya075y@gmail.com>
2022-09-04 19:15:20 +0300

    fix error chanhes file test_main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,6 +2,6 @@ from .base_page import BasePage #импорт базового класса Base
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage): #делаем класс MainPage наследником класса BasePage. Предок в скобках()
-def go_to_login_page(self): #указываем аргумент self, чтобы иметь доступ к атриб и методам класса
-    login_link = browser.find_element(By.CSS_SELECTOR. "#login_link")
-    login_link.click()
+    def go_to_login_page(self): #указываем аргумент self, чтобы иметь доступ к атриб и методам класса
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
+from .pages.main_page import MainPage #импортируем класс описывающий главную стр
 from selenium.webdriver.common.by import By
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link) #инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    # browser.get(link)
+    # login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    # login_link.click()
+    page.open() #открываем страницу, используя метод open
+    page.go_to_login_page() #выполняем метод стр - переходим на стр логина
+    
