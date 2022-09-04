1ac948330953805e07cc462cd0985ad0ee1af938
unknown <vanya075y@gmail.com>
2022-09-04 17:50:30 +0300

    add new class BasePage

---
+++ pages/main_page.py
@@ -0,0 +1,7 @@
+from .base_page import BasePage #импорт базового класса BasePage
+from selenium.webdriver.common.by import By
+
+class MainPage(BasePage): #делаем класс MainPage наследником класса BasePage. Предок в скобках()
+def go_to_login_page(self): #указываем аргумент self, чтобы иметь доступ к атриб и методам класса
+    login_link = browser.find_element(By.CSS_SELECTOR. "#login_link")
+    login_link.click()
